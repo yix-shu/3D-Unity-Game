55fa365870ee0a209e86b481b9e3d5e7737ee6ad
yix-shu <yix.shu723@gmail.com>
2022-01-26 18:48:21 -0800

    Setting up camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset; //this will be the distance the camera maintains from the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate() //runs a little after
+    {
+        transform.position = player.position + offset;
+    }
+}
