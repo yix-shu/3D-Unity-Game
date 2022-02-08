49b698c8a154c32ab68c734a4044d76119f07318
yix-shu <yix.shu723@gmail.com>
2022-02-07 23:29:58 -0800

    Changed force values again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forwardForce = 100f;
+    public float forwardForce = 1000f;
+    public float sideForce = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,11 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(0, 0, forwardForce * Time.deltaTime); //constant forward movement
         if (Input.GetKey("d"))
         {
-            rb.AddForce(500 * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //ForceMode will allow us to specific which way to add a force
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //ForceMode will allow us to specific which way to add a force
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
