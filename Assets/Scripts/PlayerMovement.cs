e25c6aa45e64525301dda31413f4b3ae5c11e41e
yix-shu <yix.shu723@gmail.com>
2022-02-07 23:24:28 -0800

    Changed speed to be slower

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forwardForce = 2000f;
+    public float forwardForce = 100f;
     // Start is called before the first frame update
     void Start()
     {
