a5b72180ba838e25b349e1c4beb9a2aebcda1fcf
yix-shu <yix.shu723@gmail.com>
2022-02-21 11:43:31 -0800

    Adding a scoreboard and displaying player position on text UI

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Text scoreboard;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreboard.text = playerPosition.position.z.ToString();
+    }
+}
